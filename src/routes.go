be0c75540657a29a3ece5a8c240a578b6a0acf38
acartagena <acartagena@salesforce.com>
2021-07-28 12:11:47 -0300

    add post to tests email addon

--- src/routes.go
+++ src/routes.go
@@ -1,6 +1,7 @@
 package src
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"my_heroku_app/src/tools"
@@ -18,6 +19,7 @@ type Routes []Route
 
 var routes = Routes {
 	Route{"Health", "GET", "/ping", Ping},
+	Route{"Health", "POST", "/email", MyPostEndpoint},
 }
 
 func NewRouter() *mux.Router {
@@ -28,7 +30,30 @@ func NewRouter() *mux.Router {
 	return router
 }
 
+func buildResponse(w http.ResponseWriter, httpStatus int, response interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatus)
+	json.NewEncoder(w).Encode(response)
+}
+
 func Ping(w http.ResponseWriter, r *http.Request) {
 	tools.Log()
 	fmt.Fprintf(w, "pong")
+}
+
+
+func MyPostEndpoint(w http.ResponseWriter, r *http.Request) {
+	for key, value := range r.Header {
+		fmt.Println(key, ":", value)
+	}
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	var object interface{}
+	err := decoder.Decode(&object)
+	if err != nil {
+		buildResponse(w, http.StatusServiceUnavailable, nil )
+		return
+	}
+	fmt.Println(object)
+	buildResponse(w, http.StatusCreated, object )
 }
